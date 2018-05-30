d1ed7b56830d564c891ceb8b05e3811a3498a56d
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-05-29 20:50:27 -0700

    Update initial mix.exs for 0.0.1

--- mix.exs
+++ mix.exs
@@ -1,28 +1,41 @@
-defmodule Quick.MixProject do
+defmodule Modglobal.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
   def project do
     [
-      app: :quick,
-      version: "0.1.0",
+      app: :modglobal,
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/AnilRedshift/quick",
+      docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.18.3", only: :dev},
+    ]
+  end
+
+  defp description do
+    "JSON-optimized web server for elixir"
+  end
+
+  defp package do
+    [
+      maintainers: ["Anil Kulkarni"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/AnilRedshift/quick"}
     ]
   end
 end
